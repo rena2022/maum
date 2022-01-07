08f9f5cee8383b9ac1f8795187d88f74f1892eb2
Rena_방희연 <rena.2021@lifeoasis.com>
2022-01-07 10:29:31 +0900

    Feat : Certification 페이지 UI

--- src/Pages/Certification.tsx
+++ src/Pages/Certification.tsx
@@ -1,31 +1,49 @@
-import React from 'react';
-import { Pressable, StyleSheet, View } from 'react-native';
+import React, { useState } from 'react';
+import { Pressable, StyleSheet, View, Text } from 'react-native';
 import Typography from '../Components/Typography';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
 import Timer from '../Components/Timer';
+import RoundBtn from '../Components/RoundBtn';
+import PinInput from '../Components/PinInput';
 
 const Certification = () => {
+  const [isFilled, setFilled] = useState(false);
+
   return (
     <View style={styles.flexAlign}>
-      {/* 헤더 */}
+      {/* Header */}
       <View style={styles.rowAlign}>
         <Pressable>
           <Typography
             value="< 이전"
-            TextStyle={{ color: '#007AFF' }}
+            textStyle={{ color: '#007AFF' }}
             containerStyle={styles.beforeBtn}
           />
         </Pressable>
         <Typography
           value="1 / 3"
-          TextStyle={{ color: '#B6B9BF', fontWeight: '700' }}
+          textStyle={{ color: '#B6B9BF', fontWeight: '700' }}
           containerStyle={styles.page}
         />
       </View>
       {/* 메인 */}
       <Typography value="인증번호 입력" type="title" />
       <Typography value="전화번호" type="subTitle" />
-      <Timer/>
+      <Timer />
+      <PinInput setFilled={setFilled} />
+      {/* Bottom */}
+      <View style={[{ justifyContent: 'space-around' }, styles.rowAlign]}>
+        <View style={styles.spotlight}>
+          <Text style={styles.spotlightTxt}>인증 문자가 오지 않나요?</Text>
+        </View>
+        <RoundBtn
+          value="확인"
+          disabled={isFilled ? false : true}
+          onPress={() => {
+            console.log('hello');
+          }}
+          containerStyle={{ opacity: isFilled ? 1 : 0.3 }}
+        />
+      </View>
     </View>
   );
 };
@@ -57,6 +75,22 @@ const styles = StyleSheet.create({
     fontWeight: '500',
     color: '#FF787E',
     marginTop: 10,
+  },
+
+  spotlight: {
+    backgroundColor: '#EAEAEA',
+    width: 139,
+    height: 30,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: 10,
+  },
+
+  spotlightTxt: {
+    color: '#777777',
+    fontSize: 12,
+    fontWeight: '700',
   },
 });
 export default Certification;
