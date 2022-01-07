531363a8f3bf1ef17a0ee06686491420206cc4fd
Rena_방희연 <rena.2021@lifeoasis.com>
2022-01-07 10:28:20 +0900

    Feat : certification 페이지 필요 컴포넌트 생성

--- src/Components/Timer.tsx
+++ src/Components/Timer.tsx
@@ -1,38 +1,69 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import Typography from './Typography';
 
 const Timer = () => {
-  const [min, setMin] = useState(3);
-  const [sec, setSec] = useState(0);
-  const time = useRef(180);
   const timerId = useRef<NodeJS.Timer | null>(null);
+  const [count, setCount] = useState(180);
+
+  // 01, 02 등 두 자리 수 반환 함수
+  function numberPad(n: number, width: number) {
+    const num = n + '';
+    return num.length >= width
+      ? num
+      : new Array(width - num.length + 1).join('0') + num;
+  }
 
   useEffect(() => {
-    timerId.current = setInterval(() => {
-      setMin(Math.floor(time.current / 60));
-      setSec(Math.floor(time.current % 60));
-      time.current -= 1;
-    }, 1000);
+    if (count > 0) {
+      timerId.current = setTimeout(() => {
+        setCount(count => count - 1);
+      }, 1000);
+    }
 
     return () => {
       timerId.current && clearInterval(timerId.current);
     };
-  }, []);
+  }, [count]);
 
   useEffect(() => {
     // 만약 타임 아웃이 발생했을 경우
-    if (time.current < 0) {
-      console.log('타임 아웃');
+    if (count < 0) {
       timerId.current && clearInterval(timerId.current);
-      // dispatch event
     }
-  }, [sec]);
+  }, [count]);
 
-  return (
-    <Text>
-      {min} 분 {sec} 초
-    </Text>
-  );
+  if (count <= 0) {
+    return (
+      <Pressable onPress={() => setCount(180)}>
+        <Typography value="인증 문자 다시 받기" textStyle={styles.mmsStyle} />
+      </Pressable>
+    );
+  } else {
+    return (
+      <Text style={styles.timerStyle}>
+        {numberPad(Math.floor(count / 60), 2)} :{' '}
+        {numberPad(Math.floor(count % 60), 2)}
+      </Text>
+    );
+  }
 };
+
+const styles = StyleSheet.create({
+  timerStyle: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: '#FF787E',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+  mmsStyle: {
+    fontSize: 14,
+    fontWeight: '500',
+    color: '#007AFF',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+});
 
 export default Timer;
