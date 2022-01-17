2e060660ca80d159d6b07b11e88b2c929c0dbca2
Rena_방희연 <rena.2021@lifeoasis.com>
2022-01-17 18:25:09 +0900

    Feat : auth 백엔드 세팅

---
+++ src/Services/AuthService.ts
@@ -0,0 +1,27 @@
+import { IAuthRepository } from '../Repositories/AuthRepository/AuthRepository';
+
+class AuthService {
+  authRepository: IAuthRepository;
+
+  constructor(AuthRepository: IAuthRepository) {
+    this.authRepository = AuthRepository;
+  }
+
+  async verifyPhoneNum(countryCode: string, phoneNum: string) {
+    return await this.authRepository.verifyPhoneNum(countryCode, phoneNum);
+  }
+
+  async checkUser(tempToken: string) {
+    return await this.authRepository.checkUser(tempToken);
+  }
+
+  async enrollUser(tempToken: string, language: string) {
+    return await this.authRepository.enrollUser(tempToken, language);
+  }
+
+  async verifyToken(accessToken: string, refreshToken: string) {
+    return await this.authRepository.verifyToken(accessToken, refreshToken);
+  }
+}
+
+export default AuthService;
